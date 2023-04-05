c4618d47da93e8483ca9af3d4f825f0ee9b3ddfe
Tony Bingo <tonybingo19970425@outlook.com>
2023-04-05 10:30:34 -0700

    call initiate with intent

--- app/src/main/java/com/daniel/freepbx001/MainActivity.kt
+++ app/src/main/java/com/daniel/freepbx001/MainActivity.kt
@@ -9,6 +9,8 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.provider.ContactsContract.CommonDataKinds
+import android.telephony.PhoneStateListener
+import android.telephony.TelephonyManager
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
@@ -28,7 +30,7 @@ import java.util.concurrent.Executors
 class MainActivity : AppCompatActivity() {
     private var serverUp = false
     // Request code for READ_CONTACTS. It can be any number > 0.
-    private val PERMISSIONS_REQUEST_READ_CONTACTS = 100
+    private val REQUEST_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,14 +54,37 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        // Check the SDK version and whether the permission is already granted or not.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(
-                arrayOf(Manifest.permission.WRITE_CONTACTS),
-                PERMISSIONS_REQUEST_READ_CONTACTS
-            )
-            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+        var btn_check = findViewById(R.id.checkbtn) as Button
+        var statusCall = findViewById(R.id.txtCallStatus) as TextView
+        statusCall.text = "Unknown"
+
+        btn_check.setOnClickListener {
+            var telephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+            var phoneStateListener: PhoneStateListener = object : PhoneStateListener() {
+                override fun onCallStateChanged(state: Int, incomingNumber: String) {
+                    when (state) {
+                        TelephonyManager.CALL_STATE_IDLE -> {}
+                        TelephonyManager.CALL_STATE_RINGING -> {}
+                        TelephonyManager.CALL_STATE_OFFHOOK -> {}
+                    }
+                }
+            }
+        //register listener
+            telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+            statusCall.text = telephonyManager.callState.toString()
         }
+        // Check the SDK version and whether the permission is already granted or not.
+        val permissions = arrayOf(
+            Manifest.permission.READ_CONTACTS,
+            Manifest.permission.READ_PHONE_STATE
+        )
+
+        requestPermissions(permissions, REQUEST_CODE)
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+//
+//            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+//        }
     }
 
 
@@ -163,21 +188,6 @@ class MainActivity : AppCompatActivity() {
                     sendResponse(httpExchange, "Would be all messages stringified json")
                 }
                 "POST" -> {
-                    Log.d("bingo", "here0")
-
-//                    var telephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
-//                    var phoneStateListener: PhoneStateListener = object : PhoneStateListener() {
-//                        override fun onCallStateChanged(state: Int, incomingNumber: String) {
-//                            when (state) {
-//                                TelephonyManager.CALL_STATE_IDLE -> {}
-//                                TelephonyManager.CALL_STATE_RINGING -> {}
-//                                TelephonyManager.CALL_STATE_OFFHOOK -> {}
-//                            }
-//                        }
-//                    }
-                    //register listener
-//                    telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
-                    Log.d("bingo", "here1")
                     val inputStream = httpExchange.requestBody
                     val requestBody = streamToString(inputStream)
 
@@ -222,39 +232,10 @@ class MainActivity : AppCompatActivity() {
 //                        responseObj.put("isWhatsappRegistered", false)
 //                        Log.d("bingo", "here3")
 //                    }
-                    val mimeString = "vnd.android.cursor.item/vnd.com.whatsapp.voip.call"
+
                     // add contact list
                     if (addPhoneNumberToContact(this, name, phone)) {
                         responseObj.put("isAddedToContactList", true)
-
-                        var _id : Long
-                        val resolver = getApplicationContext().getContentResolver();
-                        var cursor = resolver.query(
-                            ContactsContract.Data.CONTENT_URI,
-                            null,
-                            null,
-                            null,
-                            ContactsContract.Contacts.DISPLAY_NAME
-                        );
-                        while (cursor!!.moveToNext()) {
-                            _id = cursor.getLong(cursor.getColumnIndex(ContactsContract.Data._ID));
-                            val displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.Data.DISPLAY_NAME));
-                            val mimeType = cursor.getString(cursor.getColumnIndex(ContactsContract.Data.MIMETYPE));
-
-                            Log.d("Data", _id.toString() + " "+ displayName + " " + mimeType );
-
-                            if (displayName.equals(name)) {
-                                if (mimeType.equals(mimeString)) {
-                                    Log.d("Data", "called")
-                                    val data = "content://com.android.contacts/data/" + _id;
-                                    val sendIntent = Intent();
-                                    sendIntent.setAction(Intent.ACTION_VIEW);
-                                    sendIntent.setDataAndType(Uri.parse(data), mimeString);
-                                    sendIntent.setPackage("com.whatsapp");
-                                    startActivity(sendIntent);
-                                }
-                            }
-                        }
                     }
                     else {
                         responseObj.put("isAddedToContactList", false)
@@ -267,15 +248,45 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("Range")
     fun initiateWhatsAppCall(context: Context, phoneNumber: String) {
-        val intent = Intent(Intent.ACTION_VIEW)
-        intent.data = Uri.parse("https://api.whatsapp.com/send?phone=$phoneNumber")
-        try {
-            context.startActivity(intent)
-        } catch (e: ActivityNotFoundException) {
-            val fallbackIntent = Intent(Intent.ACTION_VIEW)
-            fallbackIntent.data = Uri.parse("http://www.whatsapp.com")
-            context.startActivity(fallbackIntent)
+//        val intent = Intent(Intent.ACTION_VIEW)
+//        intent.data = Uri.parse("https://api.whatsapp.com/send?phone=$phoneNumber")
+//        try {
+//            context.startActivity(intent)
+//        } catch (e: ActivityNotFoundException) {
+//            val fallbackIntent = Intent(Intent.ACTION_VIEW)
+//            fallbackIntent.data = Uri.parse("http://www.whatsapp.com")
+//            context.startActivity(fallbackIntent)
+//        }
+        val mimeString = "vnd.android.cursor.item/vnd.com.whatsapp.voip.call"
+        var _id : Long
+        val resolver = getApplicationContext().getContentResolver();
+        var cursor = resolver.query(
+            ContactsContract.Data.CONTENT_URI,
+            null,
+            null,
+            null,
+            ContactsContract.Contacts.DISPLAY_NAME
+        );
+        while (cursor!!.moveToNext()) {
+            _id = cursor.getLong(cursor.getColumnIndex(ContactsContract.Data._ID));
+            val displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.Data.DISPLAY_NAME));
+            val mimeType = cursor.getString(cursor.getColumnIndex(ContactsContract.Data.MIMETYPE));
+
+            Log.d("Data", _id.toString() + " "+ displayName + " " + mimeType );
+
+//            if (displayName.equals(name)) {
+                if (mimeType.equals(mimeString)) {
+                    Log.d("Data", "called")
+                    val data = "content://com.android.contacts/data/" + _id;
+                    val sendIntent = Intent();
+                    sendIntent.setAction(Intent.ACTION_VIEW);
+                    sendIntent.setDataAndType(Uri.parse(data), mimeString);
+                    sendIntent.setPackage("com.whatsapp");
+                    startActivity(sendIntent);
+                }
+//            }
         }
     }
 
